621774726156477844ee0e5fec333537ee854740
Mohamed TELLI <tellimohamed91@gmail.com>
2025-08-30 16:14:18 +0200

    Refactor attack logic to use predefined damage values and improve game summary output

--- main.py
+++ main.py
@@ -14,15 +14,22 @@ print("\n\n" , "+" * len(initmsg), "\n", initmsg, "\n", "+" * len(initmsg))
 ####################################################
 
 print()
-fp_attack_damage = int(input(fpname + " how much damage do you deal to " + spname + " ? "))
-sphp -= fp_attack_damage
-fp_attack_message = fpname + " attacks " + spname + " who loses " + str(fp_attack_damage) + " HP"
-fp_attack_message_2 = spname + " has now " + str(sphp) + " HP"
-width = max(len(fp_attack_message), len(fp_attack_message_2))
+attack = input(fpname + " which attack would you like to use ?\n1. Charge (-20 PV)\n2. Tonnerre (-50 PV)\n> ")
+if attack == "1" or attack == "Charge":
+  attack_damage = 20 
+elif attack == "2" or attack == "Tonnerre":
+  attack_damage = 50
+else:
+  print("Invalid value")
+  attack_damage = 0
+sphp -= attack_damage
+attack_message = fpname + " attacks " + spname + " who loses " + str(attack_damage) + " HP"
+attack_message_2 = spname + " has now " + str(sphp) + " HP"
+width = max(len(attack_message), len(attack_message_2))
 print()
 print("+"*(width+4))
-print("+", fp_attack_message + " " * (width - len(fp_attack_message)), "+")
-print("+", fp_attack_message_2 + " " * (width - len(fp_attack_message_2)), "+")
+print("+", attack_message + " " * (width - len(attack_message)), "+")
+print("+", attack_message_2 + " " * (width - len(attack_message_2)), "+")
 print("+" * (width+4))
 
 ####################################################
@@ -30,15 +37,22 @@ print("+" * (width+4))
 ####################################################
 
 print()
-sp_attack_damage = int(input(spname + " how much damage do you deal to " + fpname + " ? "))
-fphp -= sp_attack_damage
-sp_attack_message = spname + " attacks " + fpname + " who loses " + str(sp_attack_damage) + " HP"
-sp_attack_message_2 = fpname + " has now " + str(fphp) + " HP"
-width = max(len(sp_attack_message), len(sp_attack_message_2))
+attack = input(spname + " which attack would you like to use ?\n1. Charge (-20 PV)\n2. Tonnerre (-50 PV)\n> ")
+if attack == "1" or attack == "Charge":
+  attack_damage = 20 
+elif attack == "2" or attack == "Tonnerre":
+  attack_damage = 50
+else:
+  print("Invalid value")
+  attack_damage = 0
+fphp -= attack_damage
+attack_message = spname + " attacks " + fpname + " who loses " + str(attack_damage) + " HP"
+attack_message_2 = fpname + " has now " + str(fphp) + " HP"
+width = max(len(attack_message), len(attack_message_2))
 print()
 print("+"*(width+4))
-print("+", sp_attack_message + " " * (width - len(sp_attack_message)), "+")
-print("+", sp_attack_message_2 + " " * (width - len(sp_attack_message_2)), "+")
+print("+", attack_message + " " * (width - len(attack_message)), "+")
+print("+", attack_message_2 + " " * (width - len(attack_message_2)), "+")
 print("+" * (width+4))
 print()
 
@@ -51,9 +65,20 @@ fphp_msg = fpname + " has " + str(fphp) + " HP"
 sphp_msg = spname + " has " + str(sphp) + " HP"
 w_summary = max(len(fphp_msg), len(sphp_msg))
 w_summary = max(w_summary, len(summary_msg))
+if fphp == sphp : result = "Draw"
+else:
+  if fphp > sphp :
+    winner = fpname
+  elif fphp < sphp :
+    winner = spname
+  result = winner + " win the fight"
+
+w_summary = max(w_summary, len(result))
 print()
 print("+" * (w_summary + 4))
 print("+ " + summary_msg + (" " * (w_summary - len(summary_msg))) + " +")
 print("+ " + fphp_msg + (" " * (w_summary - len(fphp_msg))) + " +")
 print("+ " + sphp_msg + (" " * (w_summary - len(sphp_msg))) + " +")
+print("+ " + result + (" " * (w_summary - len(result))) + " +")
 print("+" * (w_summary + 4))
+
